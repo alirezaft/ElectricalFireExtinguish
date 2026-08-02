0325222a492a5d5f1393754721fdc029ed912ed0
Alireza Forghani Toosi <alirezaft98@gmail.com>
2026-08-02 14:01:50 +0330

    feat Add player looking

---
+++ Assets/PlayerLookController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLookController : MonoBehaviour
+{
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private float sensitivity = 0.12f;
+
+    private InputAction lookAction;
+
+    private float pitch;
+
+    private void Awake()
+    {
+        lookAction = InputSystem.actions.FindAction("Look");
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        var look = lookAction.ReadValue<Vector2>() * sensitivity;
+
+        SetPitch(look);
+        SetYaw(look);
+        
+        cameraPivot.localRotation = Quaternion.Euler(pitch, 0f, 0f);
+    }
+
+    private void SetPitch(Vector2 mouseDelta)
+    {
+        pitch -= mouseDelta.y;
+        pitch = Mathf.Clamp(pitch, -80f, 80f);
+    }
+
+    private void SetYaw(Vector2 mouseDelta)
+    {
+        transform.Rotate(Vector3.up * mouseDelta.x);
+    }
+}
