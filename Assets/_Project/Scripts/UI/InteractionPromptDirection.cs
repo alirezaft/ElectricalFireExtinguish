25d7381df3c1ec62e728dd9e32a9c79984d749e3
Alireza Forghani Toosi <alirezaft98@gmail.com>
2026-08-08 11:44:55 +0330

    fix Fix tool interaction prompt positioning problem

--- Assets/_Project/Scripts/UI/InteractionPromptDirection.cs
+++ Assets/_Project/Scripts/UI/InteractionPromptDirection.cs
@@ -5,14 +5,6 @@ namespace UI
 {
     public class InteractionPromptDirection : MonoBehaviour
     {
-        [SerializeField] private float distanceFromObject;
-        
-
-        private void OnEnable()
-        {
-            transform.position = transform.parent.position + (Vector3.up * distanceFromObject);
-        }
-
         private void LateUpdate()
         {
             transform.forward = Camera.main.transform.forward;
